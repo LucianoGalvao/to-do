d2bb59f324d4cd2d0ebaa3cd792e9868da431d53
Luciano <luciano.g.galvaao@gmail.com>
2025-01-08 17:34:35 -0300

    feat: cria array de tasks e a chamada para criacao de task

--- server.js
+++ server.js
@@ -8,9 +8,36 @@ const PORT = 3000;
 // Usando um middleware (intermediário) para processar a requisição
 app.use(express.json());
 
+// Seta a lista de atividades como um array vazio
+let tasks = [];
+
 // Setando a rota inicial
 app.get("/", (req, res) => {
   res.send("To-Do List API");
+});
+
+// Rota para criacao de uma task
+app.post("/tasks", (req, res) => {
+  const { title, description } = req.body;
+
+  // Validacao basica se ha um titulo
+  if (!title) {
+    return res.status(400).json({ error: 'O campo "title", é obrigatório!' });
+  }
+  // Inicializador default para as tasks
+  const newTask = {
+    id: tasks.length + 1,
+    title,
+    description: description || "",
+    status: "Pendente",
+    createdAt: new Date(),
+  };
+
+  // Adiciona a nova task ao array de tasks
+  tasks.push(newTask);
+
+  // Retorna a nova task criada
+  res.status(201).json(newTask);
 });
 
 // Iniciando o servidor
