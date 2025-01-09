a4aab1dd9c5307e3ba554167d7262ccacbe372e6
Luciano <luciano.g.galvaao@gmail.com>
2025-01-08 21:57:42 -0300

    feat: atualiza rotas para o mongodb

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = 3000;
 // Importa funçao de DB
 const connectDB = require("./db");
 const Task = require("./models/tasks");
+const mongoose = require("mongoose");
 
 // Conecta ao MongoDB
 connectDB();
@@ -48,54 +49,72 @@ app.post("/tasks", async (req, res) => {
 });
 
 // Rota para listar todas as tasks
-app.get("/tasks/", (req, res) => {
-  if (tasks.length === 0) {
-    return res.status(200).json({ status: "Sem tarefas cadastradas" });
+app.get("/tasks/", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    if (tasks.length === 0) {
+      return res.status(200).json({ status: "Sem tarefas cadastradas" });
+    } else {
+      res.status(200).json(tasks);
+    }
+  } catch (err) {
+    res.status(500).json({ error: "Erro ao buscar tarefas" });
   }
-  res.json(tasks);
 });
 
 // Rota para buscar uma task por id
-app.get("/tasks/:id", (req, res) => {
+app.get("/tasks/:id", async (req, res) => {
   const { id } = req.params;
-  const task = tasks.find((t) => t.id === parseInt(id));
-
-  // Validacao de task
-  if (!task) {
-    return res.status(404).json({ error: "Tarefa não encontrada" });
+  try {
+    const task = await Task.findById(id);
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ error: "Tarefa não encontrada" });
+    }
+    res.status(200).json(task);
+  } catch (err) {
+    if (err.name === "CastError") {
+      return res.status(400).json({ error: "ID Invalido" });
+    } else {
+      res.status(500).json({ error: err });
+    }
   }
-
-  res.json(task);
 });
 
 // Rota para atualizar uma task
-app.put("/tasks/:id", (req, res) => {
+app.put("/tasks/:id", async (req, res) => {
   const { id } = req.params;
   const { title, description, status } = req.body;
 
-  const task = tasks.find((t) => t.id === parseInt(id));
-  // Validacao
-  if (!task) {
-    return res.status(404).json({ error: "Tarefa não encontrada" });
-  }
-  // Atualizacao, se ha o dado, substui
-  if (title) task.title = title;
-  if (description) task.description = description;
-  if (status) task.status = status;
+  try {
+    const task = await Task.findByIdAndUpdate(
+      id,
+      { title, description, status },
+      { new: true }
+    );
 
-  res.json(task);
+    if (!task) {
+      return res.status(404).json({ error: "Tarefa não encontrada" });
+    }
+    res.status(200).json(task);
+  } catch (err) {
+    res.status(500).json({ error: "Erro ao atualizar tarefa" });
+  }
 });
 
 // Rota para deletar uma task
-app.delete("/tasks/:id", (req, res) => {
+app.delete("/tasks/:id", async (req, res) => {
   const { id } = req.params;
-  const index = tasks.findIndex((t) => t.id === parseInt(id));
 
-  if (index === -1) {
-    return res.status(404).json({ error: "Tarefa não encontrada" });
+  try {
+    const task = await Task.findOneAndDelete(id);
+    if (!task) {
+      return res.status(404).json({ error: "Tarefa não encontrada" });
+    }
+
+    res.status(202).json({ message: "Tarefa removida com sucesso" });
+  } catch (err) {
+    res.status(500).json({ error: "Erro ao deletar tarefa" });
   }
-  tasks.splice(index, 1);
-  res.status(204).send();
 });
 
 // Iniciando o servidor
