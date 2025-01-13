95e5a61c37a6636fdb04b83d58dffc802530c5a4
Luciano <luciano.g.galvaao@gmail.com>
2025-01-12 22:15:42 -0300

    feat: filtro para pagina, limite, titulo descricao e status

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -25,22 +25,38 @@ const createTask = async (req, res) => {
 };
 
 const getTasks = async (req, res) => {
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, title, description, status } = req.query;
+
+  const filter = {};
+
+  if (title) {
+    filter.title = { $regex: title, $options: "i" }; // Faz ser case sensitive
+  }
+  if (description) {
+    filter.description = { $regex: description, $options: "i" };
+  }
+  if (status) {
+    filter.status = status;
+  }
+
   try {
-    const tasks = await Task.find()
-      .skip((page - 1) * limit)
-      .limit(limit);
-    const totalTasks = await Task.countDocuments();
+    const tasks =
+      Object.keys(filter).length === 0
+        ? await Task.find()
+            .skip((page - 1) * limit)
+            .limit(limit)
+        : await Task.find(filter)
+            .skip((page - 1) * limit)
+            .limit(limit);
+    const totalTasks = await Task.countDocuments(filter);
     if (tasks.length === 0) {
       return res.status(200).json({ status: "Sem tarefas cadastradas" });
     } else {
-      res
-        .status(200)
-        .json({
-          tasks,
-          totalPages: Math.ceil(totalTasks / limit),
-          currentPage: parseInt(page),
-        });
+      res.status(200).json({
+        tasks,
+        totalPages: Math.ceil(totalTasks / limit),
+        currentPage: parseInt(page),
+      });
     }
   } catch (err) {
     res.status(500).json({ error: "Erro ao buscar tarefas" + err });
