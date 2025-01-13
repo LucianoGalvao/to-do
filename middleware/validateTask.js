e9f0ddbe13f59d31a10331b4db8e3fd5249b8004
Luciano <luciano.g.galvaao@gmail.com>
2025-01-12 23:26:19 -0300

    feat: valida se nome da task é no max 100

--- middleware/validateTask.js
+++ middleware/validateTask.js
@@ -1,7 +1,10 @@
 const { body } = require("express-validator");
 
 const validateTask = [
-  body("title").notEmpty().withMessage("O campo 'title'é obrigatório!"),
+  body("title")
+    .isLength({ max: 100 })
+    .notEmpty()
+    .withMessage("O campo 'title'é obrigatório!"),
   body("description")
     .isLength({ max: 200 })
     .withMessage("A descrição deve ter no máximo 200 caracteres."),
